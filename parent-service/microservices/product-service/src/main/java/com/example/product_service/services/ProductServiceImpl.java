42e9737bbf2e179c03a018df935d15b16be9496a
Ankit Khetan <akkhetan28@gmail.com>
2025-04-09 09:56:47 +0530

    verified

--- parent-service/microservices/product-service/src/main/java/com/example/product_service/services/ProductServiceImpl.java
+++ parent-service/microservices/product-service/src/main/java/com/example/product_service/services/ProductServiceImpl.java
@@ -2,8 +2,6 @@ package com.example.product_service.services;
 
 import com.example.api.core.product.Product;
 import com.example.api.core.product.ProductService;
-import com.example.api.exceptions.InvalidInputException;
-import com.example.api.exceptions.NotFoundException;
 import com.example.util.http.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,16 +21,16 @@ public class ProductServiceImpl implements ProductService {
     this.serviceUtil = serviceUtil;
   }
 
-  @Override
+ @Override
   public Product getProduct(int productId) {
     LOG.debug("/product return the found product for productId={}", productId);
 
     if (productId < 1) {
-      throw new InvalidInputException("Invalid productId: " + productId);
+      throw new com.example.util.exceptions.InvalidInputException("Invalid productId: " + productId);
     }
 
     if (productId == 13) {
-      throw new NotFoundException("No product found for productId: " + productId);
+      throw new com.example.util.exceptions.NotFoundException("No product found for productId: " + productId);
     }
 
     return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
